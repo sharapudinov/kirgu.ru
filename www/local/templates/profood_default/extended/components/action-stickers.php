27dc6895b4fb3c12ffc284e18da5c3f10fb090d3
artshar <sharapudinov@mail.ru>
2018-11-22 15:59:46 +0300

    stiker discount prefix

--- www/local/templates/profood_default/extended/components/action-stickers.php
+++ www/local/templates/profood_default/extended/components/action-stickers.php
@@ -18,21 +18,6 @@ Loc::loadMessages(__FILE__);
 
 <div class="c-stickers"><?
 
-    ?><span class="c-stickers__sticker c-stickers__da2"><?= Loc::getMessage('STICKERS.DA2') ?></span><?
-    ?><span class="c-stickers__sticker c-stickers__qb"><?= Loc::getMessage('STICKERS.QB') ?></span><?
-
-    ?><?php foreach (array_merge($arItem['PROPERTIES']['AKTSII']['VALUE_EXT'],$arItem['PROPERTIES']['RASSROCHKA']['VALUE_EXT'])as $value):
-
-        ?><?
-        ?><?php if (is_array($value)): ?><?
-        ?>
-        <span
-        class="c-stickers__sticker c-stickers__sticker-standart c-stickers__<?= $value['UF_XML_ID'] ?>"<?php if ($value['UF_DESCRIPTION'] != ''): ?>
-            style="background-color: <?= $value['UF_DESCRIPTION'] ?>;"<?php endif; ?>><a href="<?=$value['UF_LINK']?>"> <?= $value['UF_NAME'] ?></a>
-        </span><?
-        ?><?php endif; ?><?
-        ?><?php endforeach; ?><?
-
     ?><?php if ($arParams['STICKERS_DISCOUNT_VALUE'] == 'Y'):
         if ($arParams['USE_PRICE_COUNT'] != 'Y') {
             $val = $product['MIN_PRICE']['DISCOUNT_DIFF_PERCENT'] ;
@@ -47,7 +32,24 @@ Loc::loadMessages(__FILE__);
         ?><?
         ?><span
         class="c-stickers__sticker c-stickers__sticker-standart c-stickers__discount js-discount-value" <?php if ($val < 1): ?> style="display: none;"<?php endif; ?>>
-        -<span class="js-discount-value"><?= $val ?></span>%</span><?
+        <?=Loc::getMessage('STICKERS.DP')?><span class="js-discount-value"> <?= $val ?></span>%</span><?
         ?><?php endif; ?><?
+
+    ?>
+
+    <span class="c-stickers__sticker c-stickers__da2"><?= Loc::getMessage('STICKERS.DA2') ?></span><?
+    ?><span class="c-stickers__sticker c-stickers__qb"><?= Loc::getMessage('STICKERS.QB') ?></span><?
+
+    ?><?php foreach (array_merge($arItem['PROPERTIES']['AKTSII']['VALUE_EXT'],$arItem['PROPERTIES']['RASSROCHKA']['VALUE_EXT'])as $value):
+
+        ?><?
+        ?><?php if (is_array($value)): ?><?
+        ?>
+        <span
+        class="c-stickers__sticker c-stickers__sticker-standart c-stickers__<?= $value['UF_XML_ID'] ?>"<?php if ($value['UF_DESCRIPTION'] != ''): ?>
+            style="background-color: <?= $value['UF_DESCRIPTION'] ?>;"<?php endif; ?>><a href="<?=$value['UF_LINK']?>"> <?= $value['UF_NAME'] ?></a>
+        </span><?
+        ?><?php endif; ?><?
+        ?><?php endforeach; ?><?
 
     ?></div>
