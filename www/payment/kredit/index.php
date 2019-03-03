7267b5d32a50980cdcd346eaad4364229d76e9bf
artshar <sharapudinov@mail.ru>
2019-03-04 00:36:40 +0300

    vitrina

--- www/payment/kredit/index.php
+++ www/payment/kredit/index.php
@@ -1,4 +1,25 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Рассрочка/Кредит");?>
-    Текст страницы...
+<div class="container">
+    <div class="description">
+        Информаци ок редитных продуктах являетс ознакомительной
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <img class="credit-logo"src=""\>
+        </div>
+        <div class="col-lg-6 collapse-button">
+                <a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    Дополнительная информация
+                </a>
+            <div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
