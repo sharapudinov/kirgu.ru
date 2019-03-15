8a1aa865f928918c3a38d2ab748bd828d296408a
artshar <sharapudinov@mail.ru>
2019-03-15 12:50:12 +0300

    breadcrabs

--- www/payment/kredit/index.php
+++ www/payment/kredit/index.php
@@ -1,25 +1,138 @@
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Рассрочка/Кредит");?>
-<div class="container">
-    <div class="description">
-        Информаци ок редитных продуктах являетс ознакомительной
-    </div>
-    <div class="row">
-        <div class="col-lg-6">
-            <img class="credit-logo"src=""\>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Рассрочка/Кредит"); ?>
+    <div class="container">
+        <div class="description">
+            Ля Риба Финанс
         </div>
-        <div class="col-lg-6 collapse-button">
-                <a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                    Дополнительная информация
-                </a>
-            <div class="collapse" id="collapseExample">
-                <div class="card card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+        <div class="row">
+            <div class="col-lg-6">
+                <a href="http://www.lariba.ru/"><img width="94" height="40" alt="02343a04e9adab57a9d8b1af4e916bbc.jpg"
+                                                     src="/upload/medialibrary/ab8/ab865c534c0025009c51a1b11228ef5d.jpg"
+                                                     title="02343a04e9adab57a9d8b1af4e916bbc.jpg"></a>&nbsp;
+            </div>
+            <div class="col-lg-6 collapse-button">
+                <a data-toggle="collapse" href="#lariba" role="button" aria-expanded="false"
+                   aria-controls="lariba">
+                    Дополнительная информация </a>
+                <div class="collapse" id="lariba">
+                    <div class="card card-body">
+                        Оформить товар в рассрочку/кредит вы можете в следующих офисах Киргу: <br>
+                        1. г. Махачкала, Карабудахкенсткое шоссе д1<br>
+                        2. г. Махачкала, Карабудахкентское шоссе д11<br>
+                        3. г. Дербент,&nbsp;трасса Ростов-Баку, 938 км<br>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="description">
+            КИФ
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <img class="credit-logo" alt="киф.jpg"
+                     src="/upload/medialibrary/1b2/1b2abfb3e3c0801015ebd1cc1dd42766.jpg"
+                     width="94" height="40"title="киф.jpg">
+            </div>
+            <div class="col-lg-6 collapse-button">
+                <a data-toggle="collapse" href="#kif" role="button" aria-expanded="false"
+                   aria-controls="kif">
+                    Дополнительная информация </a>
+                <div class="collapse" id="kif">
+                    <div class="card card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+                        squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente
+                        ea proident.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="description">
+            &nbsp; Ренесанс&nbsp;
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <img class="credit-logo" alt="ренесанс.jpg" width="94" height="40"
+                     src="/upload/medialibrary/cba/cbadfb5fa16d215be34428606ce612b6.jpg"
+                      title="ренесанс.jpg">
+            </div>
+            <div class="col-lg-6 collapse-button">
+                <a data-toggle="collapse" href="#renesans" role="button" aria-expanded="false"
+                   aria-controls="renesans">
+                    Дополнительная информация </a>
+                <div class="collapse" id="renesans">
+                    <div class="card card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+                        squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente
+                        ea proident.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="description">
+            &nbsp; ОТП&nbsp;
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <img class="credit-logo" width="94" height="40" alt="02343a04e9adab57a9d8b1af4e916bbc.jpg"
+                     src="/upload/medialibrary/8e8/8e860b37e3fe11630f679e7f5d1c50cd.jpg"
+                     title="02343a04e9adab57a9d8b1af4e916bbc.jpg">
+            </div>
+            <div class="col-lg-6 collapse-button">
+                <a data-toggle="collapse" href="#otp" role="button" aria-expanded="false"
+                   aria-controls="otp">
+                    Дополнительная информация </a>
+                <div class="collapse" id="otp">
+                    <div class="card card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                        Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+                        proident.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="description">
+            &nbsp; Тинькоф
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <img class="credit-logo" alt="тинькоф.jpg" width="94" height="40"
+                     src="/upload/medialibrary/591/591713bd5bd3c950dadcf0d3297a59f3.jpg"
+                     title="тинькоф.jpg">
+            </div>
+            <div class="col-lg-6 collapse-button">
+                <a data-toggle="collapse" href="#tinkoff" role="button" aria-expanded="false"
+                   aria-controls="tinkoff">
+                    Дополнительная информация </a>
+                <div class="collapse" id="tinkoff">
+                    <div class="card card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                        Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+                        proident.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="description">
+           Почта Банк
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <img class="credit-logo" alt="02343a04e9adab57a9d8b1af4e916bbc.jpg"
+                     src="/upload/medialibrary/b9d/b9d1225ac6108a285e98ccc964bababf.jpg" width="94" height="40"
+                     title="02343a04e9adab57a9d8b1af4e916bbc.jpg">
+            </div>
+            <div class="col-lg-6 collapse-button">
+                <a data-toggle="collapse" href="#pochtabank" role="button" aria-expanded="false"
+                   aria-controls="pochtabank">
+                    Дополнительная информация </a>
+                <div class="collapse" id="pochtabank">
+                    <div class="card card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                        Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+                        proident.
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
